5b41df1e6418780ff96951987a8a3abda7e621b7
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-07-22 23:18:16 -0700

    0.1.0

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -23,8 +23,13 @@ function makeCodeRun(options) {
                 meta = JSON.parse(metasrc);
             })
             var vel = document.getElementById("version");
-            if (meta.version && vel)
-                vel.innerText = "v" + meta.version;
+            if (meta.version && meta.repo && vel) {
+                var ap = document.createElement("a");
+                ap.download = "arcade.uf2";
+                ap.href = "https://github.com/" + meta.repo + "/releases/download/v" + meta.version + "/arcade.uf2";
+                ap.innerText = "v" + meta.version;
+                vel.appendChild(ap);
+            }
             // load simulator with correct version
             document.getElementById("simframe")
                 .setAttribute("src", meta.simUrl);
@@ -78,8 +83,11 @@ function makeCodeRun(options) {
                     startSim();
                     break;
                 case "setstate":
-                    simState[d.stateKey] = d.stateValue
-                    simStateChanged = true
+                    if (d.stateValue === null)
+                        delete simState[d.stateKey];
+                    else
+                        simState[d.stateKey] = d.stateValue;
+                    simStateChanged = true;
                     break;
             }
         }
